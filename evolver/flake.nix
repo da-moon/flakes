eb9468a023d74d55b4a8975f5ee9df2aff01f0dc
da-moon <contact@havi.dev>
2026-05-07 09:04:11 +0000

    chore(evolver): bump to 1.80.1

--- evolver/flake.nix
+++ evolver/flake.nix
@@ -25,14 +25,14 @@
         lib = pkgs.lib;
         nodejs = pkgs.nodejs_22;
         pname = "evolver";
-        version = "1.79.1";
+        version = "1.80.1";
 
         npmDeps = pkgs.stdenv.mkDerivation {
           name = "${pname}-${version}-npm-deps";
 
           src = pkgs.fetchurl {
             url = "https://registry.npmjs.org/@evomap/evolver/-/evolver-${version}.tgz";
-            hash = "sha256-EpIjDPKg5jcqBWsaysEerJuZijZbnYwxH2frU4dX9NY=";
+            hash = "sha256-qf9jORiEgNsf27BZMUz3N5xmplE+HCwvIAUoGGjMWF0=";
           };
 
           nativeBuildInputs = [
@@ -45,7 +45,7 @@
 
           outputHashAlgo = "sha256";
           outputHashMode = "recursive";
-          outputHash = "sha256-+tGw1rsSQa1Nu0m1ZaiZ0FKbKfyoUO8d6+2LFo1KHAU=";
+          outputHash = "sha256-SDUmC7nF4FYerbPZgPFBDd01KX4xH/u+Z3meO0L4mmk=";
 
           buildPhase = ''
             runHook preBuild
