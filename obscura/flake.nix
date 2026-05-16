cf8db0f9347e2bc497d4cb75f5e3950df2dd8fc4
da-moon <contact@havi.dev>
2026-05-16 15:27:53 +0000

    chore(obscura): bump to 0.1.5

--- obscura/flake.nix
+++ obscura/flake.nix
@@ -20,7 +20,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
-        version = "0.1.2";
+        version = "0.1.5";
 
         obscura = pkgs.stdenv.mkDerivation rec {
           pname = "obscura";
@@ -28,7 +28,7 @@
 
           src = pkgs.fetchurl {
             url = "https://github.com/h4ckf0r0day/obscura/releases/download/v${version}/obscura-x86_64-linux.tar.gz";
-            hash = "sha256-MHzliv/qP/OSI79XPPORyIg+CBHTbpzXGF+MfFSUKAI=";
+            hash = "sha256-j5/GAYt/hLyK9hI9nuxwnIupmHWiPT4uzVwcYRak2II=";
           };
 
           sourceRoot = ".";
