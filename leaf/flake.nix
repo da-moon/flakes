30c19dce766a989b016e6ed4ef5a17955c05b56e
da-moon <contact@havi.dev>
2026-05-16 15:27:29 +0000

    chore(leaf): 1.21.0 -> 1.22.1

--- leaf/flake.nix
+++ leaf/flake.nix
@@ -23,16 +23,16 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         lib = pkgs.lib;
-        version = "1.21.0";
+        version = "1.22.1";
 
         releaseBySystem = {
           "x86_64-linux" = {
             asset = "leaf-linux-x86_64";
-            hash = "sha256-SIXioz2ChZpRmWZwH0+Fp/Ye6tAbODQiFzxWPtWCmCU=";
+            hash = "sha256-t7cKypY4N3KyaArU4ZrX9nKNuQkOTxuNaq+1WQQu4A0=";
           };
           "aarch64-linux" = {
             asset = "leaf-linux-arm64";
-            hash = "sha256-DDUmlq+O8cYA3KVyEyyDytVOvOiUbZ8qTkg7f83GUTQ=";
+            hash = "sha256-M0sU0Jm2cgosvGedj0D23OPRkm8U4A3vzAbLh/0mssE=";
           };
         };
 
